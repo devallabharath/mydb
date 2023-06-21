ad66ee0455ce8084438a17d310d7829522b05b68
Devalla <devallabharath619@gmail.com>
2023-06-20 20:12:16 -0400

    some asthetics

--- src/js/header.js
+++ src/js/header.js
@@ -8,12 +8,12 @@ export async function _themeDefault() {
 
     if (!theme || theme == 'dark') {
         localStorage.setItem('theme', 'dark')
-        themeToggler.innerHTML = '<i class="fa-solid fa-moon"></i>'
-        themeToggler.setAttribute('title', 'change to Light Theme')
+        themeToggler.innerHTML = '<i class="fa-solid fa-sun"></i>'
+        themeToggler.setAttribute('title', 'Light Theme')
     } else {
         localStorage.setItem('theme', 'light')
-        themeToggler.innerHTML = '<i class="fa-solid fa-sun"></i>'
-        themeToggler.setAttribute('title', 'change to Dark Theme')
+        themeToggler.innerHTML = '<i class="fa-solid fa-moon"></i>'
+        themeToggler.setAttribute('title', 'Dark Theme')
     }
 
     await window.theme.default(theme ? theme : 'dark')
@@ -23,12 +23,12 @@ export async function themeToggle() {
     const theme = await window.theme.toggle()
     if (theme) {
         localStorage.setItem('theme', 'dark')
-        themeToggler.innerHTML = '<i class="fa-solid fa-moon"></i>'
-        themeToggler.setAttribute('title', 'change to Light Theme')
+        themeToggler.innerHTML = '<i class="fa-solid fa-sun"></i>'
+        themeToggler.setAttribute('title', 'Light Theme')
     } else {
         localStorage.setItem('theme', 'light')
-        themeToggler.innerHTML = '<i class="fa-solid fa-sun"></i>'
-        themeToggler.setAttribute('title', 'change to Dark Theme')
+        themeToggler.innerHTML = '<i class="fa-solid fa-moon"></i>'
+        themeToggler.setAttribute('title', 'Dark Theme')
     }
 }
 
